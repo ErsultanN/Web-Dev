15de677e7651d8ed291d6b09fe35cae7a014ae08
ErsultanN <ersultan.git@gmail.com>
2025-02-23 19:51:50 +0500

    lab5

--- lab5/shop-project/src/app/app.module.ts
+++ lab5/shop-project/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MyShopComponent } from './my-shop/my-shop.component';
-import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyShopComponent,
-    HeaderComponent
+    ProductListComponent,
+    ProductItemComponent,
   ],
   imports: [
     BrowserModule,
